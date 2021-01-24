8959f35cec00abee68fb9cebc4403477a97b29a6
Luke Andrews <l.j.andrews.uk@gmail.com>
2021-01-24 02:37:54 +0000

    Fucking Finally

--- static/janalysis.js
+++ static/janalysis.js
@@ -7,7 +7,25 @@ var investD = {
   VSCGX: 10,
   VASGX: 10,
   VWIGX: 10
+};
+
+var careerIndustry = "";
+var currentSalary = 20000;
+var t0 = 2020;
+var n = 10;
+
+var cleanLabels =[];
+var cleanSalary =[];
+
+
+tn = t0 + n;
+var i;
+// Generate Labels
+for (i = t0; i < tn; i++) {
+  cleanLabels.push(String(i));
+  cleanSalary.push(i*2 + currentSalary)
 }
+
 
 
 function cons(num, min, max){
@@ -23,7 +41,7 @@ var randomScalingFactor = function() {
   return Math.round(Math.random() * 100);
 };
 
-var config = {
+var configFund = {
   type: 'doughnut',
   data: {
     datasets: [{
@@ -61,7 +79,8 @@ var config = {
     },
     title: {
       display: true,
-      text: 'Investment Distribution'
+      text: 'Investment Distribution',
+      fontSize: 30,
     },
     animation: {
       animateScale: true,
@@ -70,20 +89,17 @@ var config = {
   }
 };
 
-window.onload = function() {
-  var ctx = document.getElementById('FundProportion-area').getContext('2d');
-  window.myDoughnut = new Chart(ctx, config);
-};
+
 
 //  VFIFX
 document.getElementById('g1plus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VFIFX'] = cons(investD['VFIFX'] + 1, 0 , 20);
   dataset.data[0] = investD['VFIFX'];
   window.myDoughnut.update();
 });
 document.getElementById('g1minus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VFIFX'] = cons(investD['VFIFX'] - 1, 0 , 20);
   dataset.data[0] = investD['VFIFX'];
   window.myDoughnut.update();
@@ -91,13 +107,13 @@ document.getElementById('g1minus').addEventListener('click', function() {
 
 //  VMMSX
 document.getElementById('g2plus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VMMSX'] = cons(investD['VMMSX'] + 1, 0 , 20);
   dataset.data[1] = investD['VMMSX'];
   window.myDoughnut.update();
 });
 document.getElementById('g2minus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VMMSX'] = cons(investD['VMMSX'] - 1, 0 , 20);
   dataset.data[1] = investD['VMMSX'];
   window.myDoughnut.update();
@@ -105,13 +121,13 @@ document.getElementById('g2minus').addEventListener('click', function() {
 
 //  VSCGX
 document.getElementById('g3plus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VSCGX'] = cons(investD['VSCGX'] + 1, 0 , 20);
   dataset.data[2] = investD['VSCGX'];
   window.myDoughnut.update();
 });
 document.getElementById('g3minus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VSCGX'] = cons(investD['VSCGX'] - 1, 0 , 20);
   dataset.data[2] = investD['VSCGX'];
   window.myDoughnut.update();
@@ -119,13 +135,13 @@ document.getElementById('g3minus').addEventListener('click', function() {
 
 //  VASGX
 document.getElementById('g4plus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VASGX'] = cons(investD['VASGX'] + 1, 0 , 20);
   dataset.data[3] = investD['VASGX'];
   window.myDoughnut.update();
 });
 document.getElementById('g4minus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VASGX'] = cons(investD['VASGX'] - 1, 0 , 20);
   dataset.data[3] = investD['VASGX'];
   window.myDoughnut.update();
@@ -133,96 +149,162 @@ document.getElementById('g4minus').addEventListener('click', function() {
 
 //  VWIGX
 document.getElementById('g5plus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VWIGX'] = cons(investD['VWIGX'] + 1, 0 , 20);
   dataset.data[4] = investD['VWIGX'];
   window.myDoughnut.update();
 });
 document.getElementById('g5minus').addEventListener('click', function() {
-  var dataset = config.data.datasets[0];
+  var dataset = configFund.data.datasets[0];
   investD['VWIGX'] = cons(investD['VWIGX'] - 1, 0 , 20);
   dataset.data[4] = investD['VWIGX'];
   window.myDoughnut.update();
 });
 
+var color = Chart.helpers.color;
 
-/*
+var configSal = {
+  type: 'line',
+  data: {
+    labels: cleanLabels,
+    datasets: [{
+      data: cleanSalary,
+      backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+      borderColor: window.chartColors.red,
+      type: 'line',
+      fill: true,
+    }]
+  },
+  options: {
+    responsive: true,
+    legend: {
+      display: false
+    },
+    title: {
+      display: true,
+      text: 'Salary Progression',
+      fontSize: 30
+    },
+    tooltips: {
+      enabled: false,
+      mode: 'index',
+      intersect: false,
+    },
+    hover: {
+      //enabled: false,
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Year'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'GNI'
+        },
+        ticks: {
+          min: 0
+        }
+      }]
+    }
+  }
+};
 
-document.getElementById('randomizeData').addEventListener('click', function() {
-  config.data.datasets.forEach(function(dataset) {
-    dataset.data = dataset.data.map(function() {
-      return randomScalingFactor();
-    });
-  });
 
-  window.myDoughnut.update();
+// Industry select
+document.getElementById('tech').addEventListener('click', function() {
+  careerIndustry = "Tech";
+  document.getElementById('bindtext').textContent = "Tech";
+  updateSalary();
+});
+document.getElementById('finance').addEventListener('click', function() {
+  careerIndustry = "Finance";
+  document.getElementById('bindtext').textContent = "Finance";
+
+  updateSalary();
+});
+document.getElementById('other').addEventListener('click', function() {
+  careerIndustry = "Other";
+  document.getElementById('bindtext').textContent = "Other";
+  updateSalary();
+});
+document.getElementById('salaryBox').addEventListener('focusout', function() {
+  currentSalary =  parseInt(document.getElementById('salaryBox').value);
+  console.log(document.getElementById('salaryBox').value);
+  updateSalary();
 });
 
-*/
+function updateSalary(){
+  tn = t0 + n;
+  var labels = [];
+  var i;
 
-/*
-var colorNames = Object.keys(window.chartColors);
-document.getElementById('addDataset').addEventListener('click', function() {
-  var newDataset = {
-    backgroundColor: [],
-    data: [],
-    label: 'New dataset ' + config.data.datasets.length,
-  };
-
-  for (var index = 0; index < config.data.labels.length; ++index) {
-    newDataset.data.push(randomScalingFactor());
-
-    var colorName = colorNames[index % colorNames.length];
-    var newColor = window.chartColors[colorName];
-    newDataset.backgroundColor.push(newColor);
+  // Generate Labels
+  for (i = t0; i < tn; i++) {
+    labels.push(String(i));
   }
 
-  config.data.datasets.push(newDataset);
-  window.myDoughnut.update();
-});
+  var a,b,c,d,e;
 
-document.getElementById('addData').addEventListener('click', function() {
-  if (config.data.datasets.length > 0) {
-    config.data.labels.push('data #' + config.data.labels.length);
-
-    var colorName = colorNames[config.data.datasets[0].data.length % colorNames.length];
-    var newColor = window.chartColors[colorName];
-
-    config.data.datasets.forEach(function(dataset) {
-      dataset.data.push(randomScalingFactor());
-      dataset.backgroundColor.push(newColor);
-    });
-
-    window.myDoughnut.update();
+  if (careerIndustry == "Tech"){
+    a = 1.722;
+    b = -164.483;
+    c = 5102.335;
+    d = 30544.535;
+    e = 30000;
   }
-});
-
-document.getElementById('removeDataset').addEventListener('click', function() {
-  config.data.datasets.splice(0, 1);
-  window.myDoughnut.update();
-});
-
-document.getElementById('removeData').addEventListener('click', function() {
-  config.data.labels.splice(-1, 1); // remove the label first
-
-  config.data.datasets.forEach(function(dataset) {
-    dataset.data.pop();
-    dataset.backgroundColor.pop();
-  });
-
-  window.myDoughnut.update();
-});
-
-document.getElementById('changeCircleSize').addEventListener('click', function() {
-  if (window.myDoughnut.options.circumference === Math.PI) {
-    window.myDoughnut.options.circumference = 2 * Math.PI;
-    window.myDoughnut.options.rotation = -Math.PI / 2;
-  } else {
-    window.myDoughnut.options.circumference = Math.PI;
-    window.myDoughnut.options.rotation = -Math.PI;
+  if (careerIndustry == "Finance"){
+    a = -42.170;
+    b = 2003.89;
+    c = -5752.958;
+    d = 66581.237;
+    e = 55000;
+  }
+  else
+  {
+    a = 2.044;
+    b = -137.947;
+    c = 399.435;
+    d = 22896;
+    e = 24000;
   }
 
-  window.myDoughnut.update();
-});
+  var salary = [currentSalary];
+  var j;
+  // Generate sal
+  for (j = 1; j < n; j++) {
+    x = (a*j**3 +b*j**2 + c*j + d) * (currentSalary/24000);
+    salary.push(x);
+  }
 
-*/
+
+
+  var dataset = configSal.data.datasets[0];
+  dataset.label = labels;
+  dataset.data = salary;
+  //dataset.label = ["A","B","C"];
+  //dataset.data = [1,2,3];
+  window.myLine.update();
+
+  console.log(labels, salary);
+}
+
+
+// OnLoad
+
+window.onload = function(){
+  var ctx = document.getElementById('SalaryProp-area').getContext('2d');
+  window.myLine = new Chart(ctx, configSal);
+
+  var ctx = document.getElementById('FundProportion-area').getContext('2d');
+  window.myDoughnut = new Chart(ctx, configFund);
+
+  //updateSalary();
+};
